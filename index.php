951cb5e48cf207e84f86b83242ee1b653a221773
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-07-08 10:12:36 +0200

    correzioni

--- index.php
+++ index.php
@@ -8,25 +8,69 @@ class Movies {
 
     public function __construct($title, $type, $duration, $pegi)
     {
-        echo $this->title = $title;
-        echo $this->type = $type;
-        echo $this->duration = $duration;
-        echo $this->pegi = $pegi;
-        echo $this->movies_duration();
+        $this->title = $title;
+        $this->type = $type;
+        $this->duration = $duration;
+        $this->pegi = $pegi;
+        $this->movies_pegi(15);
     }
 
-    public function movies_duration(){
-       if ($this->pegi < 18) {
-            echo '<br>troppo piccolo per il film</br>';
+
+
+    public function movies_pegi($eta){
+       if ($eta < $this->pegi) {
+            return '<br>troppo piccolo per il film</br>';
        } 
        else{
-            echo '<br>goditi il film</br>';
+            return '<br>goditi il film</br>';
        }
+    }
+
+    public function print_card($eta){
+        return "<h1> {$this->title}</h1>
+        <h3> type: {$this->type}</h3>
+        <div> {$this->duration} min</div>" .
+        $this->movies_pegi($eta);
     }
 }
 
 
-$Movie1 = new Movies('<h1>Title: Star Wars</h1>', '<h3>type: Fantasy</h3>', '<div>duration: 120 min</div>', 18);
-$Movie2 = new Movies('<h1>Title: Spider man</h1>', '<h3>type: Hero</h3>', '<div>duration:100 min</div>', 9);
-$Movie3 = new Movies('<h1>Title: bruce lee</h1>', '<h3>type: combattimento</h3>', '<div>duration:80 min</div>', 3);
+$Movie1 = new Movies('Star Wars', 'Fantasy', 120, 18);
+$Movie2 = new Movies('Spider man', 'Hero', 100,  9);
+$Movie3 = new Movies('bruce lee', 'combattimento', 80 , 3);
  
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><?php
+
+   echo $Movie1->print_card(10);
+   echo $Movie2->print_card(10);
+   echo $Movie3->print_card(10);
+
+    ?>
+    <h1><?= $Movie1->title?></h1>
+    <h3>Type: <?= $Movie1->type?></h3>
+    <div><?= $Movie1->duration ?> min</div>
+    <?= $Movie1-> movies_pegi(20)?>
+
+
+    <h1>Title: <?= $Movie2->title?></h1>
+    <h3>Type: <?= $Movie2->type?></h3>
+    <div><?= $Movie2->duration ?> min</div>
+    <?= $Movie2-> movies_pegi(20)?>
+
+    <h1>Title: <?= $Movie3->title?></h1>
+    <h3>Type: <?= $Movie3->type?></h3>
+    <div><?= $Movie3->duration?> min</div>
+</body>
+</html>
